c76c22719a6f987a2885471bd2c142b02d8c3a5a
Collin Sigismondi <50307582+sigismondi@users.noreply.github.com>
2019-06-29 07:42:34 -0400

    fixed issues but not still fully funtional
    ran the issues through in chrome dev tools but is getting an error from giphy.

--- gify.js
+++ gify.js
@@ -16,14 +16,14 @@ renderButtons();
 
 $("#add-char").on("click", function () {
     event.preventDefault();
-    var bbchar = $("#charinput").val().trim();
+    var bbchar = $("#char-input").val().trim();
     bbchar.push(bbchar);
     renderButtons();
     return;
 });
 
 $("button").on("click", function () {
-    var bbchar = $(this).attr("chardata");
+    var bbchar = $(this).attr("data-sponge");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         bbchar + "2szW9Epf5GDJMa6k4XoPZcUkBGfjygLL"
 
@@ -31,36 +31,36 @@ $("button").on("click", function () {
         url: queryURL,
         method: "GET"
     }).done(function (response) {
-        var results = response.data;
-        $("#bbchar").empty();
-        for (var i = 0; i < results.length; i++) {
-        var gifDiv = $("<div>");
-        var p = $("<p>").text("Rating: " + results[i].rating);
-        var Img = $("<img>");
-        Img.attr("src", results[i].images.original_still.url);
-        Img.attr("data-still", results[i].images.original_still.url);
-        Img.attr("data-animate", results[i].images.original.url);
-        Img.attr("data-state", "still");
-        Img.attr("class", "gif");
-        gifDiv.append(p);
-        gifDiv.append(Img);
-        $("#bbchar").append(gifDiv);
+    var results = response.data;
+    $("#bbchar").empty();
+    for (var i = 0; i < results.length; i++) {
+    var gifDiv = $("<div>");
+    var p = $("<p>").text("Rating: " + results[i].rating);
+    var Img = $("<img>");
+    Img.attr("src", results[i].images.original_still.url);
+    Img.attr("data-still", results[i].images.original_still.url);
+    Img.attr("data-animate", results[i].images.original.url);
+    Img.attr("data-state", "still");
+    Img.attr("class", "gif");
+    gifDiv.append(p);
+    gifDiv.append(Img);
+    $("#bbchar").append(gifDiv);
         }
     });
 });
 
 function changeState(){
-       var state = $(this).attr("data-state");
-       var animateImage = $(this).attr("data-animate");
-       var stillImage = $(this).attr("data-still");
-       if (state == "still") {
-         $(this).attr("src", animateImage);
-         $(this).attr("data-state", "animate");
-    }
-    else if (state == "animate") {
-        $(this).attr("src", stillImage);
-        $(this).attr("data-state", "still");
-    }
+    var state = $(this).attr("data-state");
+    var animateImage = $(this).attr("data-animate");
+    var stillImage = $(this).attr("data-still");
+    if (state == "still") {
+      $(this).attr("src", animateImage);
+      $(this).attr("data-state", "animate");
+ }
+ else if (state == "animate") {
+     $(this).attr("src", stillImage);
+     $(this).attr("data-state", "still");
+ }
 }
 
 $(document).on("click", ".gif", changeState);
