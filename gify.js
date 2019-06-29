e75657273a2e06ca79141901a62a465c070cb272
Collin Sigismondi <50307582+sigismondi@users.noreply.github.com>
2019-06-29 05:35:00 -0400

    added ajax call and giphy api

--- gify.js
+++ gify.js
@@ -4,3 +4,15 @@ $(document).ready(function() {
 
 var bbchar = ["Spongebob", "Patrick", "Squidward", "Sandy", "Mr Krabs", "Plankton",];
 
+
+function displayInfo() {
+    var spongebob = $(this).attr("sponge-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + spongebob + "";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+    }).done(function(response) {
+    ("#pineapple").empty();
+    var results = response.data;
+
