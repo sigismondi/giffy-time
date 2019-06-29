0e22e38e4bf0bfeed3126a5bf788442b93b8eab2
Collin Sigismondi <50307582+sigismondi@users.noreply.github.com>
2019-06-29 07:03:02 -0400

    app is not working in current state
    added in stop start functionality for gifs, added in api key from giphy. appended jqry to html.

--- gify.js
+++ gify.js
@@ -7,7 +7,7 @@ var bbchar = ["Spongebob", "Patrick", "Squidward", "Sandy", "Mr Krabs", "Plankto
 function renderButtons() {
 
     $("#sponge-buttons").empty();
-    for (i = 0; i < movies.length; i++) {
+    for (i = 0; i < bbchar.length; i++) {
     $("#sponge-buttons").append("<button class='btn btn-success' data-sponge='" + bbchar[i] + "'>" + bbchar[i] + "</button>");
     }
 }
@@ -16,8 +16,52 @@ renderButtons();
 
 $("#add-char").on("click", function () {
     event.preventDefault();
-    var bbchar = $("#char-input").val().trim();
+    var bbchar = $("#charinput").val().trim();
     bbchar.push(bbchar);
     renderButtons();
     return;
+});
+
+$("button").on("click", function () {
+    var bbchar = $(this).attr("chardata");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        bbchar + "2szW9Epf5GDJMa6k4XoPZcUkBGfjygLL"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+        var results = response.data;
+        $("#bbchar").empty();
+        for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
+        var p = $("<p>").text("Rating: " + results[i].rating);
+        var Img = $("<img>");
+        Img.attr("src", results[i].images.original_still.url);
+        Img.attr("data-still", results[i].images.original_still.url);
+        Img.attr("data-animate", results[i].images.original.url);
+        Img.attr("data-state", "still");
+        Img.attr("class", "gif");
+        gifDiv.append(p);
+        gifDiv.append(Img);
+        $("#bbchar").append(gifDiv);
+        }
+    });
+});
+
+function changeState(){
+       var state = $(this).attr("data-state");
+       var animateImage = $(this).attr("data-animate");
+       var stillImage = $(this).attr("data-still");
+       if (state == "still") {
+         $(this).attr("src", animateImage);
+         $(this).attr("data-state", "animate");
+    }
+    else if (state == "animate") {
+        $(this).attr("src", stillImage);
+        $(this).attr("data-state", "still");
+    }
+}
+
+$(document).on("click", ".gif", changeState);
 });
